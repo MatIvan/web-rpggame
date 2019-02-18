f7739a02538f248cb228653e985a713f1c840ea4
MatIvan <matukin.ivan@gmail.com>
2019-02-19 00:52:33 +0300

    Перенёс подключение БЕМ-css в отдельный файл. Немного поправил дизайн

--- game/battle.php
+++ game/battle.php
@@ -7,52 +7,72 @@ include('../navbar.php');
 	<div class="main-block__caption">
 	Битва:
 	</div>
-	
-	<!-- Проверка регистрации -->
-	<?php 
-	if( empty($_SESSION['user_id']) ){
-		message_box( "Необходимо войти на сайт.", "/index.php", "На главную" ); 
-		alarm_exit();
-	}
-	if ( $_POST['id_warrior']===NULL ){
-		message_box( "Не выбран воин для битвы.", "/game/select_apponent.php", "Назад" );
-		alarm_exit();
-	}
-	if( $_POST['id_apponent']===NULL ){
-		message_box( "Не выбран аппонент для битвы.", "/game/select_apponent.php", "Назад" );
-		alarm_exit();
-	}
-	?>
+	<div class="battle-form">
 
-	<!-- Получить данные бойцов из БД -->
-	<?php
-	if ( ! $warrior = get_warrior_by_id( $_POST['id_warrior'] ) ) {
-		message_box( "Боец не найден в БД.", "/game/select_apponent.php", "Назад" ); 
-		alarm_exit();
-	}
-	if ( ! $apponent = get_warrior_by_id( $_POST['id_apponent'] ) ) {
-		message_box( "Аппонент не найден в БД.", "/game/select_apponent.php", "Назад" ); 
-		alarm_exit();
-	}
-	$warrior_show = $warrior;
-	include("warrior_big_form.php");
-	
-	?>
-	<div class="versus">
-		<img style="float: left;" src='/img/versus.png' alt="VERSUS">
-	</div>
+		<!-- Проверка регистрации -->
+		<?php 
+		if( empty($_SESSION['user_id']) ){
+			message_box( "Необходимо войти на сайт.", "/index.php", "На главную" ); 
+			alarm_exit();
+		}
+		if ( $_POST['id_warrior']===NULL ){
+			message_box( "Не выбран воин для битвы.", "/game/select_apponent.php", "Назад" );
+			alarm_exit();
+		}
+		if( $_POST['id_apponent']===NULL ){
+			message_box( "Не выбран аппонент для битвы.", "/game/select_apponent.php", "Назад" );
+			alarm_exit();
+		}
+		?>
 
-	<?php
-		$warrior_show = $apponent;
+		<!-- Получить данные бойцов из БД -->
+		<?php
+		if ( ! $warrior = get_warrior_by_id( $_POST['id_warrior'] ) ) {
+			message_box( "Боец не найден в БД.", "/game/select_apponent.php", "Назад" ); 
+			alarm_exit();
+		}
+		if ( ! $apponent = get_warrior_by_id( $_POST['id_apponent'] ) ) {
+			message_box( "Аппонент не найден в БД.", "/game/select_apponent.php", "Назад" ); 
+			alarm_exit();
+		}
+		
+		$warrior_show = $warrior;
 		include("warrior_big_form.php");
-	?>
+		
+		?>
+		<img class="battle-form__versus" src='/img/versus.png' alt="VERSUS">
 
-	<!-- Рассчитать сражение -->
+		<?php
+			$warrior_show = $apponent;
+			include("warrior_big_form.php");
+			echo("</div>");
+		?>
+		<div style="clear: both;">&nbsp;</div>
 
-	<!-- Записать результаты в БД -->
+		<!-- Рассчитать сражение -->
+		<?php
+		$max_steps = 10;
+		while ( $warrior["hp"]>0 && $apponent["hp"]>0 && $max_steps>0 ) {
+			$max_steps--;
+			
+			$hit_w_a = $warrior["attack"] - ( ($apponent["shield"] * $apponent["shield"] ) / $warrior["attack"] );
+			$hit_a_w = $apponent["attack"] - ( ($warrior["shield"] * $warrior["shield"] ) / $apponent["attack"] );
+			$hit_w_a = round($hit_w_a, 0);
+			$hit_a_w = round($hit_a_w, 0);
+			if($hit_w_a<1) $hit_w_a=1;
+			if($hit_a_w<1) $hit_a_w=1;
+			
+			!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+			!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-	<!-- Записать лог боя в БД -->
+			echo("<br> / ".$hit_w_a." / ".$hit_a_w);
+		}
+		?>
 
+		<!-- Записать результаты в БД -->
+
+		<!-- Записать лог боя в БД -->
+	</div>
 </main>
 
 <?php
--- header.php
+++ header.php
@@ -13,43 +13,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>RPG web game.</title>
 	
-	
-    <link rel="stylesheet" href="/css/main-style.css">
+	<link rel="stylesheet" href="/css/main-style.css">
+	<link rel="stylesheet" href="/css/bem-files.css">
 	<link rel="stylesheet" href="/css/flat-radio-btn.css">
-	
-	<!-- Подключем css файлы по методике BEM -->
-	<link rel="stylesheet" href="/blocks/error-msg/error-msg.css">
-	<link rel="stylesheet" href="/blocks/main-header/main-header.css">
-	
-	<link rel="stylesheet" href="/blocks/main-block/main-block.css">
-	<link rel="stylesheet" href="/blocks/main-block/main-block__caption.css">
-	
-	<link rel="stylesheet" href="/blocks/main-footer/main-footer.css">
-	
-	<link rel="stylesheet" href="/blocks/all-forms/all-forms.css">
-	<link rel="stylesheet" href="/blocks/all-forms/all-forms__caption.css">
-	<link rel="stylesheet" href="/blocks/all-forms/all-forms_login-form.css">
-	
-	<link rel="stylesheet" href="/blocks/all-forms/all-forms__input-text.css">
-	<link rel="stylesheet" href="/blocks/all-forms/all-forms__input-text_center.css">
-	
-	<link rel="stylesheet" href="/blocks/all-forms/all-forms__btn.css">
-	<link rel="stylesheet" href="/blocks/all-forms/all-forms__btn_center.css">
-	
-	<link rel="stylesheet" href="/blocks/navbar/navbar.css">
-	<link rel="stylesheet" href="/blocks/navbar/navbar_center.css">
-	<link rel="stylesheet" href="/blocks/navbar/navbar__a.css">
-
-	<link rel="stylesheet" href="/blocks/all-forms/all-forms_warriors-form.css">
-	
-	<link rel="stylesheet" href="/blocks/all-forms/all-forms__table.css">
-	<link rel="stylesheet" href="/blocks/all-forms/all-forms__table_center.css">
-
-	<link rel="stylesheet" href="/blocks/warrior-str/warrior-str.css">
-
-	<link rel="stylesheet" href="/blocks/all-forms/all-forms_warrior.css">
-	<link rel="stylesheet" href="/blocks/warrior-data/warrior-data.css">
-	
 </head>
 <body>
    
