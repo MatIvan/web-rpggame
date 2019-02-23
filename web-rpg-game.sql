8362f0f1cbc34b2566575474d2edbeee847a2190
MatIvan <matukin.ivan@gmail.com>
2019-02-23 22:23:17 +0300

    Добавил файл экспорта из БД

---
+++ web-rpg-game.sql
@@ -0,0 +1,123 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Фев 23 2019 г., 22:21
+-- Версия сервера: 5.6.41
+-- Версия PHP: 5.5.38
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `web-rpg-game`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `history`
+--
+
+CREATE TABLE `history` (
+  `id` int(11) NOT NULL,
+  `datetime` datetime NOT NULL,
+  `user_id_w` int(11) NOT NULL,
+  `name_w` varchar(25) NOT NULL,
+  `hp_w` int(11) NOT NULL,
+  `attack_w` int(11) NOT NULL,
+  `shield_w` int(11) NOT NULL,
+  `user_id_a` int(11) NOT NULL,
+  `name_a` varchar(25) NOT NULL,
+  `hp_a` int(11) NOT NULL,
+  `attack_a` int(11) NOT NULL,
+  `shield_a` int(11) NOT NULL,
+  `resut` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `login` varchar(20) NOT NULL,
+  `password` varchar(35) NOT NULL,
+  `balance` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `warriors`
+--
+
+CREATE TABLE `warriors` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `hp` int(11) NOT NULL DEFAULT '0',
+  `attack` int(11) NOT NULL DEFAULT '0',
+  `shield` int(11) NOT NULL DEFAULT '0',
+  `user_id` int(11) NOT NULL,
+  `level` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `history`
+--
+ALTER TABLE `history`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `warriors`
+--
+ALTER TABLE `warriors`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `history`
+--
+ALTER TABLE `history`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `warriors`
+--
+ALTER TABLE `warriors`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
