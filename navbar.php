5df938d37ec49fa99a1d445316041dbf759d91a6
MatIvan <matukin.ivan@gmail.com>
2019-02-25 09:43:47 +0300

    Добавил баланс на панель навигации

--- navbar.php
+++ navbar.php
@@ -1,18 +1,26 @@
-<nav class="navbar">
-	<?php if( $_SESSION['user_status'] == 1 ): ?>
-		<b>Admin</b>
-		<!-- <a class="navbar__a" href="#">Admin</a> -->
-	<?php endif;?>
-	
-	<a class="navbar__a" href="/index.php">Главная</a>
-	<a class="navbar__a" href="/instructions.php">Правила</a>
-	<?php if( empty($_SESSION['user_id']) ): ?>
-		<a class="navbar__a" href="/login/registration.php">Регистрация</a>
-	<?php else: ?>
-		<a class="navbar__a" href="/game/warriors.php">Бойцы</a>
-		<a class="navbar__a" href="/game/select_apponent.php">Сразиться</a>
-		<a class="navbar__a" href="/game/history.php">История</a></li>
-		<a class="navbar__a" href="/login/logout.php">Выход</a>
-	<?php endif ?>
-	
-</nav>
+<div class="navwrapper">
+
+	<nav class="navbar">
+		<?php if( $_SESSION['user_status'] == 1 ): ?>
+			<b>Admin</b>
+			<!-- <a class="navbar__a" href="#">Admin</a> -->
+		<?php endif;?>
+		
+		<a class="navbar__a" href="/index.php">Главная</a>
+		<a class="navbar__a" href="/instructions.php">Правила</a>
+		<?php if( empty($_SESSION['user_id']) ): ?>
+			<a class="navbar__a" href="/login/registration.php">Регистрация</a>
+		<?php else: ?>
+			<a class="navbar__a" href="/game/warriors.php">Бойцы</a>
+			<a class="navbar__a" href="/game/select_apponent.php">Сразиться</a>
+			<a class="navbar__a" href="/game/history.php">История</a></li>
+			<a class="navbar__a" href="/login/logout.php">Выход</a>
+		<?php endif ?>
+		
+	</nav>
+
+	<div class="navuser">
+		<b><?= $_SESSION['user_login'] ?></b> баланс: <?= $_SESSION['user_balance'] ?>
+	</div>
+
+</div>
